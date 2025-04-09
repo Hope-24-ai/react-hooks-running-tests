e6189f0177cbe1e70689ca04c7b81f2c8df3055c
hopeyuniawasonga <hope.wasonga@student.moringaschool.com>
2025-04-09 13:08:36 +0300

    running tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,25 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+       please pass this test 
     </div>
   );
 }
 
 export default Article;
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
